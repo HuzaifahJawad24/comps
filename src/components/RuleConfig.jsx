e5779cb30d111076aa64ffcda5ac99686ad99d80
Sufian <sufian.babri@gmail.com>
2020-12-20 18:02:25 +0500

    Refactored RuleConfig and RuleConfigurations components.

--- src/components/RuleConfig.jsx
+++ src/components/RuleConfig.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import Select from "./select";
 import TextField from "./textfield";
 import PlusCircleIcon from "../icons/plus-circle.svg";
 import MinusCircleIcon from "../icons/minus-circle.svg";
 
-const RuleConfig = ({ onAdd, onRemove}) => {
+const RuleConfig = ({onAdd, onRemove, id}) => {
 
     const populateAddOrRemoveButton = () => {
         if (onAdd) return <img src={PlusCircleIcon} alt="Add More" onClick={() => onAdd()}/>;
-        else return <img src={MinusCircleIcon} alt="Remove" onClick={() => onRemove()}/>;
+        else return <img src={MinusCircleIcon} alt="Remove" onClick={() => onRemove(id)}/>;
     }
 
     return (
-        <Container className="pb-2 mb-3" style={{border: '1px solid #AAAAAA'}}>
+        <Container key={id} className="pb-2 mb-3" style={{border: '1px solid #AAAAAA'}}>
             <Row className="mt-3" style={{
                 display: "flex",
                 justifyContent: "flex-end",
@@ -23,40 +23,40 @@ const RuleConfig = ({ onAdd, onRemove}) => {
             </Row>
             <Row>
                 <Col><Select
-                    id = "PracticeSpecific"
-                    label = "Practice Specific"
-                    selectclass = "form-control custom-select"
-                    holderclass = "form-group"
+                    id="PracticeSpecific"
+                    label="Practice Specific"
+                    selectclass="form-control custom-select"
+                    holderclass="form-group"
                     values=
                         {[
-                            { text: 'Select' },
-                            { text: 'Dummy1' },
-                            { text: 'Dummy2' }
+                            {text: 'Select'},
+                            {text: 'Dummy1'},
+                            {text: 'Dummy2'}
                         ]}
                 /></Col>
                 <Col><Select
-                    id = "PayerSpecific"
-                    label = "Payer Specific"
-                    selectclass = "form-control custom-select"
-                    holderclass = "form-group"
+                    id="PayerSpecific"
+                    label="Payer Specific"
+                    selectclass="form-control custom-select"
+                    holderclass="form-group"
                     values=
                         {[
-                            { text: 'Select' },
-                            { text: 'Dummy1' },
-                            { text: 'Dummy2' }
+                            {text: 'Select'},
+                            {text: 'Dummy1'},
+                            {text: 'Dummy2'}
                         ]}
                 /></Col>
             </Row>
             <Row className="align-items-center">
                 <Col><Select
-                    id = "Workflow"
-                    label = "Workflow"
-                    selectclass = "form-control custom-select"
+                    id="Workflow"
+                    label="Workflow"
+                    selectclass="form-control custom-select"
                     values=
                         {[
-                            { text: 'Select' },
-                            { text: 'Dummy1' },
-                            { text: 'Dummy2' }
+                            {text: 'Select'},
+                            {text: 'Dummy1'},
+                            {text: 'Dummy2'}
                         ]}
                 /></Col>
                 <Col><TextField type="text" label="Module" name="module"/></Col>
@@ -65,6 +65,6 @@ const RuleConfig = ({ onAdd, onRemove}) => {
             </Row>
         </Container>
     );
-}
+};
 
 export default RuleConfig;
--- src/components/RuleConfigurations.jsx
+++ src/components/RuleConfigurations.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import RuleConfig from "./RuleConfig";
 
 const RuleConfigurations = () => {
-    const [boxElementCount, setBoxElementCount] = useState(1);
+    const generateId = () => {
+        return new Date().getMilliseconds();
+    }
+
+    const [configs, setConfigs] = useState([{id: generateId()}]);
+    const onAdd = () => setConfigs((prev) => [...prev, {id: generateId()}]);
+    const onRemove = (id) => setConfigs((prev) => {
+        return prev.filter(e => e.id !== id);
+    });
+
+    const [first, ...rest] = configs;
+
     return (
         <div>
-            <RuleConfig onAdd={() => setBoxElementCount(boxElementCount + 1)}/>
-            {Array(boxElementCount - 1).fill(0).map(() =>
-                <RuleConfig onRemove={() => setBoxElementCount(boxElementCount - 1)}/>
+            <RuleConfig onAdd={onAdd} id={first.id}/>
+            {rest.map((e) =>
+                <RuleConfig onRemove={onRemove} id={e.id}/>
             )}
             <button className="btn btn-primary btn-sm pull-right mr-5">Save</button>
         </div>
